71d8dbbec677dca0b4fa27cff3770dd26a42f35b
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-12-07 17:40:55 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const Mastodon = require('mastodon-api');
+const cron = require('node-cron');
+
+// Replace with your access token and Mastodon instance URL
+const ACCESS_TOKEN = 'SrQRozNE5_aY0dbpcVy7tSaNNvjP6xxU1DUN6GXMpXg';
+const MASTODON_INSTANCE = 'https://botsin.space';
+
+const client = new Mastodon({
+  access_token: ACCESS_TOKEN,
+  timeout_ms: 60 * 1000,
+  api_url: `${MASTODON_INSTANCE}/api/v1/`,
+});
+
+cron.schedule('0 12 1-7 * 1', () => {
+
+  // Replace with your toot message
+  const toot = '@frank@indieweb.social This is a scheduled toot';
+  const visibility = 'direct';
+  
+  client.post('statuses', { status: toot, visibility: visibility })
+    .then(result => {
+      console.log(`Tooted: ${toot}`);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
